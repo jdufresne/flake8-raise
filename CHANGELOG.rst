b98bacb451037e7ccf8c66164bf21942ab2e06e7
Jon Dufresne <jon.dufresne@gmail.com>
2020-01-19 08:34:48 -0800

    Prepare for 0.0.3 release

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,12 @@
 Changelog
 =========
 
+0.0.3 (2020-01-19)
+------------------
+
+- Fixed malformed reStructuredText syntax in README.rst to allow upload to
+  PyPI.
+
 0.0.2 (2020-01-19)
 ------------------
 
--- flake8_raise.py
+++ flake8_raise.py
@@ -3,7 +3,7 @@ import ast
 
 class RaiseStatementChecker:
     name = "flake8-raise"
-    version = "0.0.2"
+    version = "0.0.3"
     text = "R100 raise in except handler without from."
 
     def __init__(self, tree, filename):
